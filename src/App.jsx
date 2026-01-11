be7654b0863eb08390a8cc7dbd45e3b1ccbb1857
ramizamad <ramiz@bcbgroup.io>
2026-01-11 00:52:28 +0000

    v2.2.0

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import SurahLibrary from './components/SurahLibrary';
 import { surahs as baseSurahs } from './data/surahs';
 import { getClipsForSurah } from './data/clipsManifest';
 
-const VERSION = '2.1.0';
+const VERSION = '2.2.0';
 
 function App() {
   const [currentSurah, setCurrentSurah] = useState(null);
